257b975da2a588c171f0f592ad962446f5222760
unknown <m44614234@gmail.com>
2025-07-03 15:57:22 +0330

    resolve problem in contact

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,39 +1,49 @@
-import connectToDB from "@/config/db";
-import ContactModel from "@/models/Contact";
-import { NextRequest, NextResponse } from "next/server";
+   import connectToDB from "@/config/db";
+   import ContactModel from "@/models/Contact";
+   import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
-  connectToDB();
+   export async function POST(req: NextRequest) {
+     connectToDB();
 
-  try {
-    const body = await req.json();
-    const { first_name, last_name, desc } = body;
+     try {
+       const body = await req.json();
+       const { first_name, last_name, desc } = body;
 
-    if (!req.body || !first_name || !last_name || !desc) {
-      return NextResponse.json(
-        { message: "Please fill in the required fields." },
-        { status: 422 }
-      );
-    }
+       if (!first_name || !last_name || !desc) {
+         return NextResponse.json(
+           { message: "Please fill in the required fields." },
+           { status: 422 }
+         );
+       }
 
-    const contact = await ContactModel.create({
-      first_name,
-      last_name,
-      desc,
-    });
+       const contact = await ContactModel.create({
+         first_name,
+         last_name,
+         desc,
+       });
 
-    const response = NextResponse.json(
-      { message: "Contact Created Successfully :))", data: contact },
-      { status: 201 }
-    );
+       const response = NextResponse.json(
+         { message: "Contact Created Successfully :))", data: contact },
+         { status: 201 }
+       );
 
-    response.headers.set("Access-Control-Allow-Origin", "*");
-    response.headers.set("Access-Control-Allow-Methods", "POST, OPTIONS");
-    response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+       // اضافه کردن هدرهای CORS
+       response.headers.set("Access-Control-Allow-Origin", "*");
+       response.headers.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+       response.headers.set("Access-Control-Allow-Headers", "Content-Type");
 
-    return response;
-  } catch (error: any) {
-    console.log("error =>", error);
-    return NextResponse.json({ message: "Error =>", error }, { status: 500 });
-  }
-}
+       return response;
+     } catch (error: any) {
+       console.log("error =>", error);
+       return NextResponse.json({ message: "Error =>", error }, { status: 500 });
+     }
+   }
+
+   export async function OPTIONS(req: NextRequest) {
+     const response = NextResponse.json({});
+     response.headers.set("Access-Control-Allow-Origin", "*");
+     response.headers.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+     response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+     return response;
+   }
+   
